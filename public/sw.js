4a4fc6077337642a84688f24f403da5ef4c9e4fd
= <stanleyloren@gmail.com>
2019-08-28 11:44:18 +0300

    updated sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,7 @@
+/**
+ * This file is not working as intended please vistit
+ * service worker documentation for more info and how you can make it work for you
+ */
 self.addEventListener('fetch', (event) => {
     //use the default fetch handler
     console.log('Fetch is not being done by the service worker')
