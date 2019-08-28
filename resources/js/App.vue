a95c58a33bd8cf00146cc5af06496950f76b0d46
= <stanleyloren@gmail.com>
2019-08-28 13:35:51 +0300

    The landing page has a new face

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -2,16 +2,15 @@
   <main>
     <div class="flex-center position-ref full-height">
       <div class="content">
-        <div class="title m-b-md">Laravel</div>
+        <div class="title m-b-md">Laravel Vue spa</div>
 
         <div class="links">
-          <a href="https://laravel.com/docs">Docs</a>
-          <a href="https://laracasts.com">Laracasts</a>
-          <a href="https://laravel-news.com">News</a>
-          <a href="https://blog.laravel.com">Blog</a>
-          <a href="https://nova.laravel.com">Nova</a>
-          <a href="https://forge.laravel.com">Forge</a>
-          <a href="https://github.com/laravel/laravel">GitHub</a>
+          <a href="https://laravel.com/docs">Laravel Docs</a>
+          <a href="https://vuejs.org">Vue JS</a>
+          <a href="https://router.vuejs.org">Vue Router</a>
+          <a href="https://stannlee.github.io/vue-laravel-spa/">Docs</a>
+          <a href="https://laravel-news.com">Laravel News</a>
+          <a href="https://github.com/stannlee/vue-laravel-spa">GitHub</a>
         </div>
       </div>
     </div>
