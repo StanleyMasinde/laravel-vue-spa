229037f487e0c4943af9899d57a72dbaec955dc2
= <stanleyloren@gmail.com>
2019-09-14 00:46:37 +0300

    fixed the docs demo path

--- documentation/.vuepress/config.js
+++ documentation/.vuepress/config.js
@@ -8,7 +8,7 @@ module.exports = {
         nav: [
             { text: 'Home', link: '/' },
             { text: 'Guide', link: '/guide/' },
-            { text: 'Demo', link: 'https://vue-laravel-spa.stanley-masinde.tech' }
+            { text: 'Demo', link: 'https://vue-laravel-spa.stanleymasinde.tech' }
         ], sidebar: 'auto', repo: 'stannlee/vue-laravel-spa', repoLabel: 'Contribute!', editLinks: true, editLinkText: 'Help us improve this page!'
     }, head: [
         ['meta', { name: 'theme-color', content: '#3eaf90' }]
