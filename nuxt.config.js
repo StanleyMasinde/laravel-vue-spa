5ff1f90cdce7713bfbece9a5c1050964f1854950
Stanley Masinde <stanleyloren@gmail.com>
2020-06-05 22:37:43 +0300

    Lint on save

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,4 +35,20 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+        })
+      }
+    },
+  },
 }
