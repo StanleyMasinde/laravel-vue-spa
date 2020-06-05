2f059765e1abcc3bb9b65eb4d854b38342a86437
Stanley Masinde <stanleyloren@gmail.com>
2020-06-05 21:30:32 +0300

    Basic nuxtjs scaffolding and basic configuration.

---
+++ nuxt.config.js
@@ -0,0 +1,15 @@
+export default {
+  buildDir: 'resources/.nuxt',
+  generate: {
+    dir: "resources/dist"
+  },
+  dir: {
+    assets: 'resources/assets',
+    app: 'resources/app',
+    layouts: 'resources/layouts',
+    middleware: 'resources/middleware',
+    pages: 'resources/pages',
+    static: 'resources/static',
+    store: 'resources/store'
+  }
+}
